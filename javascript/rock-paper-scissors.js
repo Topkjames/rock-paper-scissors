a2006ef28239d3c2d9ac6caf1e7b12aabebf0cf2
Topkjames <topkjames@gmail.com>
2026-02-04 22:53:58 +0100

    Created a new variable "getComputerChoice" to log the computer's choice in the console. This variable is assigned a random number between 0 and 1, which corresponds to the choices of rock, paper, and scissors. The function then uses conditional statements to determine the computer's choice based on the random number generated. Finally, the computer's choice is logged to the console for debugging purposes.

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,2 +1,15 @@
 console.log("Hello World!");
 
+function getComputerchoice() {
+    const randomValue = Math.random();
+
+    if (randomValue < 0.34) {
+        return "rock";
+    } else if (randomValue <= 0.67) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerchoice());
