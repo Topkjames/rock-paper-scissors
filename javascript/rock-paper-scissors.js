ae4faf4cd7f7d4d3f04c14e6c72b3b4959ee6f27
Topkjames <topkjames@gmail.com>
2026-02-07 01:03:16 +0100

    add humanScore and computerScore variables to keep track of the scores
    created a playRound function that takes in the human and computer choices and determines the winner of each round, updating the scores accordingly
    
    used the ++ operator to increment the humanScore and computerScore variables when a player wins a round

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,15 +1,44 @@
-console.log("Hello World!");
+console.log("Hello World");
 
-function getComputerchoice() {
-    const randomValue = Math.random();
+function computerChoice() {
+  const randomValue = Math.random();
 
-    if (randomValue < 0.34) {
-        return "rock";
-    } else if (randomValue <= 0.67) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  if (randomValue < 0.34) {
+    return "rock";
+  } else if (randomValue < 0.67) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-console.log(getComputerchoice());
+console.log(computerChoice());
+
+function getHumanChoice() {
+  const humanValue = prompt(
+    `Choose either "rock", "paper" or "scissors"`
+  ).toLowerCase();
+
+  return humanValue;
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return `It's a tie!`;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    return `You win! ${humanChoice.toUpperCase()} beats ${computerChoice}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerChoice.toUpperCase()} beats ${humanChoice}`;
+  }
+}
